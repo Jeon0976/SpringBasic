fbdc670e49729a0ba415ede6848bed55e3a95a26
Jeon Seoung Hun <jsh097610@gmail.com>
2024-03-29 00:43:46 +0900

    update: new discount policy

---
+++ src/test/java/com/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,26 @@
+package com.hello.core.order;
+
+import com.hello.core.member.Grade;
+import com.hello.core.member.Member;
+import com.hello.core.member.MemberService;
+import com.hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.assertj.core.api.Assertions;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
